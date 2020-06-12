8f8534bcc8dd50ef0a31627328fa4eb91c2e0ba9
sinhvien <sinhvien>
2020-06-12 14:50:23 +0700

    date .3

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -36,6 +36,7 @@ namespace BigSchool.Controllers
 
         [HttpPost]
         [Authorize]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(CourseViewModel viewModel)
         {
            if(!ModelState.IsValid)
--- BigSchool/ViewModels/FutureDate.cs
+++ BigSchool/ViewModels/FutureDate.cs
@@ -13,7 +13,7 @@ namespace BigSchool.ViewModels
         public override bool IsValid(object value)
         {
             DateTime dateTime;
-            var isValid = DateTime.TryParseExact(Convert.ToString(value), "dd/mm/yyyy", CultureInfo.CurrentCulture,DateTimeStyles.None,out dateTime);
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), "dd/MM/yyyy", CultureInfo.CurrentCulture,DateTimeStyles.None,out dateTime);
             return (isValid && dateTime>DateTime.Now);
         }
     }
