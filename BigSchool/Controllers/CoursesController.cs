3b3f74dcfa0b61581521fdf1d0067750dd50d832
Nguyen8676 <caongocnguyen99@gmail.com>
2020-06-16 11:15:41 +0700

    update 16/06/2020 Show following lecturer

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -74,6 +74,7 @@ namespace BigSchool.Controllers
             return View(viewModel);
         }
 
+       
         [Authorize]
         public ActionResult Mine()
         {
---
+++ BigSchool/Controllers/LecturerController.cs
@@ -0,0 +1,43 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class LecturerController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public LecturerController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Lecturer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult Following()
+        {
+            var userId = User.Identity.GetUserId();
+            var following = _dbContext.Followings.Where(a => a.FollowerId == userId)
+                                                .Select(a => a.Followee)
+                                                .ToList();
+            var viewModel = new FollowingViewModel
+            {
+                FollowingCourses = following,
+                ShowAction = User.Identity.IsAuthenticated
+
+            };
+            return View(viewModel);
+        }
+
+    }
+}
